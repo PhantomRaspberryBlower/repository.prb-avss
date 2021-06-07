fd237f1870a27c5164ed8efdf2a0f0d389f1860f
Phantom Raspberry Blower <jasonbulis@hotmail.com>
2021-06-07 23:37:43 +0100

    Updated install_avss.sh and icon

--- .av_stream/install_avss.sh
+++ .av_stream/install_avss.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 # Audio Video Streaming Service (AVSS)
-# Script for installing AVSS onto a headless
-# Raspberry Pi installed with buster lite
+# Script for installing AVSS onto a headless Raspberry Pi installed with buster lite
+# Need to make it executable and run as root ie.
+# sudo chmod +x install_avss.sh
+# followed by
+# sudo bash install_avss.sh
+# Written by: Phantom Raspberry Blower
+# Date May 2021
 
+if [ "$EUID" -ne 0 ]
+  then echo "Needs to be run as root! Type in: sudo bash install_avss.sh"
+  exit
+fi
 echo "-----------------------------------"
 echo "        Installing updates"
 echo "-----------------------------------"
-sudo apt-get -y update
+apt-get -y update
 echo "-----------------------------------"
 echo "      Upgrading installation"
 echo "-----------------------------------"
-sudo apt-get -y upgrade
+apt-get -y upgrade
 echo "-----------------------------------"
 echo "         Installing FFmpeg"
 echo "-----------------------------------"
-sudo apt-get -y install ffmpeg
+apt-get -y install ffmpeg
 echo "-----------------------------------"
 echo "        Installing festival"
 echo "-----------------------------------"
-sudo apt-get -y install festival
+apt-get -y install festival
 echo "-----------------------------------"
 echo "        Installing PiCamera"
 echo "-----------------------------------"
-sudo apt-get -y install python-picamera
-sudo apt-get -y install python3-picamera
+apt-get -y install python-picamera
+apt-get -y install python3-picamera
 echo "-----------------------------------"
 echo "         Installing PyGame"
 echo "-----------------------------------"
-sudo apt-get -y install python3-pygame
+apt-get -y install python3-pygame
 echo "-----------------------------------"
 echo "         Installing git"
 echo "-----------------------------------"
-sudo apt-get -y install git
+apt-get -y install git
 echo "-----------------------------------"
 echo "     Making working directory"
 echo "-----------------------------------"
@@ -44,26 +53,26 @@ git clone https://github.com/PhantomRaspberryBlower/repository.prb-avss /home/pi
 cp -r /home/pi/.av_stream/.av_stream/*.* /home/pi/.av_stream/
 rm -r /home/pi/.av_stream/.av_stream
 rm -r /home/pi/.av_stream/.git
-sudo cp /home/pi/.av_stream/asound.conf /etc/asound.conf
-sudo cp /home/pi/.av_stream/av_stream.service /etc/systemd/system/av_stream.service
-sudo systemctl enable /etc/systemd/system/av_stream.service
+cp /home/pi/.av_stream/asound.conf /etc/asound.conf
+cp /home/pi/.av_stream/av_stream.service /etc/systemd/system/av_stream.service
+systemctl enable /etc/systemd/system/av_stream.service
 cd /.av_stream
-sudo chown pi:pi *.*
-echo "-----------------------------------"
-echo "       Cleaning installation"
-echo "-----------------------------------"
-rm /home/pi/.av_stream/asound.conf
-rm /home/pi/.av_stream/av_stream.service
-sudo apt -y autoremove
+chown pi:pi *.*
 echo "-----------------------------------"
 echo "           Enable camera"
 echo "-----------------------------------"
-sudo raspi-config nonint do_camera 0
+raspi-config nonint do_camera 0
+echo "-----------------------------------"
+echo "       Cleanup installation"
+echo "-----------------------------------"
+rm /home/pi/.av_stream/asound.conf
+rm /home/pi/.av_stream/av_stream.service
+apt -y autoremove
 echo "==================================="
 echo "           Completed :)"
 echo "==================================="
-sudo shutdown -r
 echo ""
 echo "-----------------------------------"
 echo "           Rebooting"
 echo "-----------------------------------"
+reboot
