65b7c133f270c64f4e90f6d4caba3ddf9b329cd0
Phantom Raspberry Blower <jasonbulis@hotmail.com>
2021-06-18 02:46:51 +0100

    Update av_stream_settings.py

--- .av_stream/av_stream_settings.py
+++ .av_stream/av_stream_settings.py
@@ -354,7 +354,8 @@ try:
         txt = commontasks.regex_from_to(txt, ' "', '" ')
         camera.annotate_text = dt.datetime.now().strftime(txt)
         camera.annotate_foreground = picamera.color.Color(settings_dict['video_out_overlay_text_color'])
-        camera.annotate_background = picamera.color.Color(settings_dict['video_out_overlay_bg_color'])
+        if settings_dict['video_out_overlay_bg_color_enabled'] == 'True':
+            camera.annotate_background = picamera.color.Color(settings_dict['video_out_overlay_bg_color'])
         camera.annotate_text_size = int(font_size)
         output = StreamingOutput()
         camera.start_recording(output, format='mjpeg')
