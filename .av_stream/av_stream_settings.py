1728623e6a47b71d2a617e1e0828b95146d874e8
Phantom Raspberry Blower <jasonbulis@hotmail.com>
2021-06-21 11:46:19 +0100

    Update av_stream_settings.py

--- .av_stream/av_stream_settings.py
+++ .av_stream/av_stream_settings.py
@@ -413,7 +413,7 @@ def set_camera_settings():
         camera.annotate_background = None
     camera.annotate_text_size = int(font_size)
     print(settings_dict['video_image_automatic_white_balance'])
-    camera.AWB_MODES = settings_dict['video_image_automatic_white_balance']
+    camera.awb_mode = settings_dict['video_image_automatic_white_balance']
     camera.brightness = int(settings_dict['video_image_brightness'])
     camera.contrast = int(settings_dict['video_image_contrast'])
     camera.saturation = int(settings_dict['video_image_saturation'])
