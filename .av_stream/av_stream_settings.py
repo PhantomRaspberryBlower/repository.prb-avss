34025a7a006dd3a9726a9d9d411a3002a5d7da97
Phantom Raspberry Blower <jasonbulis@hotmail.com>
2021-07-13 13:20:39 +0100

    Update av_stream_settings.py

--- .av_stream/av_stream_settings.py
+++ .av_stream/av_stream_settings.py
@@ -86,8 +86,7 @@ def set_settings():
 
 def options(opt, lst):
   txt = ''
-#  indent = ' ' * 12
-  indent = '            '
+  indent = ' ' * 12
   for item in lst:
     if opt == item:
       txt += '\n%s<option selected="selected" value="%s">%s</option>' % (indent, item, item)
